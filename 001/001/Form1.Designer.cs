41050a082f681954a20b081b356b734a84c30773
mxx-zzz <yaxin_ma@163.com>
2021-01-10 15:49:11 +0800

    001

--- 001/001/Form1.Designer.cs
+++ 001/001/Form1.Designer.cs
@@ -28,6 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_Main));
             this.listView1 = new System.Windows.Forms.ListView();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -36,13 +38,15 @@
             this.button2 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
             this.SuspendLayout();
             // 
             // listView1
             // 
             this.listView1.HideSelection = false;
             this.listView1.Location = new System.Drawing.Point(9, 11);
-            this.listView1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.listView1.Margin = new System.Windows.Forms.Padding(5);
             this.listView1.Name = "listView1";
             this.listView1.Size = new System.Drawing.Size(829, 295);
             this.listView1.TabIndex = 0;
@@ -83,21 +87,23 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(206, 457);
+            this.button1.Location = new System.Drawing.Point(249, 457);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(95, 42);
             this.button1.TabIndex = 5;
             this.button1.Text = "登录";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(431, 457);
+            this.button2.Location = new System.Drawing.Point(520, 457);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(95, 42);
             this.button2.TabIndex = 6;
             this.button2.Text = "退出";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // textBox1
             // 
@@ -110,14 +116,32 @@
             // 
             this.textBox2.Location = new System.Drawing.Point(367, 389);
             this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
             this.textBox2.Size = new System.Drawing.Size(169, 29);
             this.textBox2.TabIndex = 8;
+            // 
+            // listView2
+            // 
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(9, 12);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(829, 294);
+            this.listView2.TabIndex = 9;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "驯鹿.png");
+            this.imageList1.Images.SetKeyName(1, "雪人.png");
             // 
             // Frm_Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(856, 527);
+            this.Controls.Add(this.listView2);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
@@ -127,9 +151,10 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.listView1);
             this.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Margin = new System.Windows.Forms.Padding(5);
             this.Name = "Frm_Main";
             this.Text = "带图像列表的系统登录程序";
+            this.Load += new System.EventHandler(this.Frm_Main_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -145,6 +170,8 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ImageList imageList1;
     }
 }
 
--- 001/001/Form1.cs
+++ 001/001/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace _001
 {
     public partial class Frm_Main : Form
@@ -15,6 +16,53 @@ namespace _001
         public Frm_Main()
         {
             InitializeComponent();
+        }
+        DataTable dataTable = new DataTable();
+        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DataRow dataRow;
+            dataRow = dataTable.NewRow();
+            dataRow["username"] = this.textBox1.Text;
+            dataTable.Rows.Add(dataRow);
+            Method(dataTable);
+
+        }
+        /// <summary>
+        /// 显示数据表中的内容
+        /// </summary>
+        /// <param name="dataTable"></param>
+        private void Method(DataTable dataTable)
+        {
+            listView2.Items.Clear();
+            for (int i = 0; i < dataTable.Rows.Count; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    listView2.Items.Add(dataTable.Rows[i][0].ToString(), 0);
+                }
+                else
+                {
+                    listView2.Items.Add(dataTable.Rows[i][0].ToString(), 1);
+                }
+
+            }
+        }
+
+        private void Frm_Main_Load(object sender, EventArgs e)
+        {
+            listView2.View = View.LargeIcon;
+            listView2.LargeImageList = imageList1;
+            DataColumn dataColumn = new DataColumn();
+            dataColumn.DataType = System.Type.GetType("System.String");
+            dataColumn.ColumnName = "userName";
+            dataTable.Columns.Add(dataColumn);
         }
     }
 }
